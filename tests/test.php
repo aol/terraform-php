672513be0312436d702e68cd20c6707d27e4b09e
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-11-02 15:31:55 -0500

    Cleanup and add better examples.

--- tests/test.php
+++ tests/test.php
@@ -5,29 +5,24 @@ require_once '../vendor/autoload.php';
 use Terraform\Blocks\Resource;
 
 $terraform = new \Terraform\Terraform();
-$terraform->hi='j';
-$nat = new Resource('aws_nat', 'my_nat');
-$nat->name1 ='name';
-echo $nat->toJson();
-$terraform->nat =$nat;
-echo $terraform->toJson();
- $terraform->dump();
-exit();
 
+$provider = new \Terraform\Blocks\Provider('aws');
+$provider->region = 'us-east-1';
+$terraform->provider = $provider;
+
+foreach (['prod', 'dev', 'staging'] as $env) {
+    $lc = new Resource('aws_launch_configuration', 'my_launch_configuration_' . $env);
+    $lc->image_id = 'ami-eb02508e';
+    $lc->instance_type = "t2.micro";
+    $lc->key_name = 'my_key';
+    $lc->lifecycle = ["create_before_destroy" => true];
+    $terraform->{"lc_$env"} = $lc;
+}
 
 $policy = new Resource('aws_iam_role_policy', 'foo_policy');
 $policy->name = "foo_policy";
 $policy->role = '${aws_iam_role.foo_role.id}';
 $policy->prop = 'prop';
-$policy->toJson();
-//var_dump($policy);
 $terraform->policy = $policy;
 
-/*$t = new Resource('aws_iam_user', 'foo_user');
-$t->name = "foo_policy";
-$t->role = '${aws_iam_role.foo_role.id}';
-$t->prop = 'prop';
-$t->toJson();
-var_dump($t);*/
-
-$terraform->dump();
+$terraform->save();
