17666897115cbcdb3f4b86e9e5dc77b71a99a60a
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-12-01 17:07:34 -0500

    Update aws_security_group macro.

--- src/Macros/Aws/Aws.php
+++ src/Macros/Aws/Aws.php
@@ -6,21 +6,23 @@ use Terraform\Blocks\Resource;
 
 class Aws
 {
-    public static function securityGroup($name, $vpcId, $cidrBlocks = ['0.0.0.0/0'], $fromPort = 0, $toPort = 0, $protocol = -1)
+    public static function securityGroup($name, $vpcId,
+                                         array $ingress = [
+                                             'cidr_blocks' => ['0.0.0.0/0'],
+                                             'from_port' => 0,
+                                             'to_port' => 0,
+                                             'protocol' => -1,
+                                         ],
+                                         array $egress = [
+                                             'cidr_blocks' => ['0.0.0.0/0'],
+                                             'from_port' => 0,
+                                             'to_port' => 0,
+                                             'protocol' => -1,
+                                         ])
     {
         $sg = new Resource('aws_security_group', $name);
-        $sg->egress = [
-            'cidr_blocks' => ['0.0.0.0/0'],
-            'from_port' => 0,
-            'to_port' => 0,
-            'protocol' => -1,
-        ];
-        $sg->ingress = [
-            'cidr_blocks' => $cidrBlocks,
-            'from_port' => $fromPort,
-            'to_port' => $toPort,
-            'protocol' => $protocol,
-        ];
+        $sg->ingress = $ingress;
+        $sg->egress = $egress;
         $sg->vpc_id = $vpcId;
         $sg->name = $name;
         $sg->description = "$name security group";
