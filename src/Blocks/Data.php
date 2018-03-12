8476efbea8804baa5754256119a295624cab363e
Ameir Abdeldayem <ameirh@gmail.com>
2018-03-12 15:35:54 -0400

    Add support for `data` blocks.

---
+++ src/Blocks/Data.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Terraform\Blocks;
+
+class Data extends Block implements BlockInterface
+{
+    public function __construct($resourceType, $resourceName)
+    {
+        parent::__construct('data', $resourceType, $resourceName);
+    }
+
+    public function toArray()
+    {
+        return [$this->_block => [$this->_type => [$this->_name => $this->_data]]];
+    }
+
+    public function getTfProp($property = 'id', $encapsulate = true)
+    {
+        $resource = "{$this->_type}.{$this->_name}.{$property}";
+        if ($encapsulate) {
+            $resource = '${' . $resource . '}';
+        }
+        return $resource;
+    }
+}
